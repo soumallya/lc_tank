f308efe776738277f234097a51bbe525a0b950b5
Soumallya Chatterjee <sbchat13@gmail.com>
2016-10-22 17:25:07 +0530

    Bash file which can handle .mp4 files

--- execute.sh
+++ execute.sh
@@ -1,14 +1,14 @@
 python main.py
+python animate.py
 pdflatex 153070018.tex
 pdflatex 153070018.tex
 bibtex 153070018.aux
 pdflatex 153070018.tex
 pdflatex 153070018.tex
-ipython nbconvert 153070018.ipynb
 
 mkdir output
 cp *.png output/
 cp *.pdf output/
 cp *.html output/
+cp *.mp4 output/
 cp *.txt output/
-
